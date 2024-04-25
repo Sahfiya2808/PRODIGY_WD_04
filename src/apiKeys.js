b8554ea96468b112648f2264fb31349594bfead9
Sahfiyathul Mahruba - SKCET <123156379+Sahfiya2808@users.noreply.github.com>
2024-04-25 15:18:15 +0530

    Add files via upload

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+    key: "bd5e378503939ddaee76f12ad7a97608",
+    base: "https://api.openweathermap.org/data/2.5/",
+  };
